0305212516564bae7cf06aabf3570eecc2dc30b6
Byron <byron@atebol.com>
2018-06-09 12:38:35 +0100

    font fixes

--- gruntfile.js
+++ gruntfile.js
@@ -41,5 +41,5 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.registerTask('default', ['less', 'cssmin','watch']);
+    grunt.registerTask('booteek', ['less', 'cssmin','watch']);
 };
