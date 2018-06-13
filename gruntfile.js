29591f6165ecf3b1a1188838a528b439071b8bac
Byron <byron@atebol.com>
2018-06-13 01:12:30 +0100

    udpate to core less

--- gruntfile.js
+++ gruntfile.js
@@ -13,7 +13,8 @@ module.exports = function (grunt) {
         cssmin: {
             options: {
                 mergeIntoShorthands: false,
-                roundingPrecision: -1
+                roundingPrecision: -1,
+                sourceMap:true
             },
             target: {
                 files: {
